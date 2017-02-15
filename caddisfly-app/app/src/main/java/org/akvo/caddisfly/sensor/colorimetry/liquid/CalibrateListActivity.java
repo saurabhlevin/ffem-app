e1c07a08dd92400426c044ac7811e00b354aa3a4
nhternup <nhternup@users.noreply.github.com>
2017-02-15 09:26:52 +0530

    Remove external camera code

--- caddisfly-app/app/src/main/java/org/akvo/caddisfly/sensor/colorimetry/liquid/CalibrateListActivity.java
+++ caddisfly-app/app/src/main/java/org/akvo/caddisfly/sensor/colorimetry/liquid/CalibrateListActivity.java
@@ -229,7 +229,11 @@ public class CalibrateListActivity extends BaseActivity
 
         if (AppPreferences.useExternalCamera() || !ApiUtil.isCameraInUse(this, null)) {
             final Intent intent = new Intent(getIntent());
-            intent.setClass(getBaseContext(), ColorimetryLiquidActivity.class);
+            if (AppPreferences.useExternalCamera()) {
+                intent.setClass(getBaseContext(), ColorimetryLiquidExternalActivity.class);
+            } else {
+                intent.setClass(getBaseContext(), ColorimetryLiquidActivity.class);
+            }
             intent.putExtra("isCalibration", true);
             intent.putExtra("swatchValue", swatch.getValue());
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
