711254046087c383d097706f9193e53a61222ff6
nhternup <nhternup@users.noreply.github.com>
2018-05-30 12:04:06 +0530

    Fix send file. Update ui tests

--- caddisfly-app/app/src/androidTestExperiment/java/org/akvo/caddisfly/instruction/StriptestInstruction.java
+++ caddisfly-app/app/src/androidTestExperiment/java/org/akvo/caddisfly/instruction/StriptestInstruction.java
@@ -79,7 +79,7 @@ import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class StriptestInstructions {
+public class StriptestInstruction {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
