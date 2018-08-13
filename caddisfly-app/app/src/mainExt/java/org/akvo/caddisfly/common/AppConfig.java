0ae21889688af80be0b8e37c05fc990e08f2d6c9
nhternup <nhternup@users.noreply.github.com>
2018-08-13 09:45:03 +0530

    Update versions details

--- caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
+++ caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
@@ -35,7 +35,7 @@ public final class AppConfig {
      */
     public static final boolean APP_EXPIRY = true;
     public static final int APP_EXPIRY_DAY = 30;
-    public static final int APP_EXPIRY_MONTH = 7;
+    public static final int APP_EXPIRY_MONTH = 8;
     public static final int APP_EXPIRY_YEAR = 2018;
 
     /**
