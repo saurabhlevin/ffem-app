bd598bc2e2d234f255cbc3d95c419aac370b276a
nhternup <nhternup@users.noreply.github.com>
2018-10-18 14:38:32 +0530

    Add Hindi translations

--- caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
+++ caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
@@ -33,7 +33,7 @@ public final class AppConfig {
      * Date on which the app version will expire.
      * This is to ensure that installs from apk meant for testing only are not used for too long.
      */
-    public static final boolean APP_EXPIRY = false;
+    public static final boolean APP_EXPIRY = true;
     public static final int APP_EXPIRY_DAY = 30;
     public static final int APP_EXPIRY_MONTH = 10;
     public static final int APP_EXPIRY_YEAR = 2018;
